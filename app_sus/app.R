1eab73690c5cca901ae1bda0a37b9d4e1c96b189
Vasco Alberto Filipe Ribeiro <vascoribeiro.sintra@gmail.com>
2021-04-03 16:30:56 +0100

    Completada a APP de atribuição de CC e Rubricas funcional

--- app_sus/app.R
+++ app_sus/app.R
@@ -1,18 +1,19 @@
+#library(shiny)
+#library(DT)
 
-library(shiny)
-library(DT)
+#library(shinymanager)
+#library(keyring)
 
-library(shinymanager)
-library(keyring)
+#library(dplyr)
 
-library(dplyr)
+#CCR <- read.csv("./CentrosdeCusto.csv", sep=";")
 
-CCR <- read.csv("./CentrosdeCusto.csv", sep=";")
+#CC <- unique(CCR$CENTRO.DE.CUSTO)
+#CCrub <- unique(CCR$RUBRICA)
 
-CC <- unique(CCR$CENTRO.DE.CUSTO)
-CCrub <- unique(CCR$RUBRICA)
+source("libs_form.R")
 
-# Define UI for application that draws a histogram
+# Define UI for application 
 ui <- fluidPage(
     titlePanel("Estudo Financeiro SUS"),
     hr(style="border-color: grey;"),
@@ -46,7 +47,7 @@ ui <- fluidPage(
         )
     )
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
     
     data <- eventReactive(input$gobutton,{
@@ -54,25 +55,30 @@ server <- function(input, output) {
             return()
         }
 
-        inFile <- input$file1
-        myDF <- read.csv(inFile$datapath, skip = input$rowsHeader, sep = input$sep)[,1:input$nCols]
-      
-        for (i in 1:nrow(myDF)) {
+      inFile <- input$file1
+      myDF <- read.csv(inFile$datapath, skip = input$rowsHeader, sep = input$sep)[,1:input$nCols]
 
-          if (is.na(myDF$CENTRO.DE.CUSTO[i]) ) { ## this assumes that a code have run before creating the column
+        
+  ## CODE THAT READS AND PERFORMS A TRANSFORMATION TO MY DF - AUTOMATISM
+      
+      myDF <- mensalFunction(myDF)
+
+        
+      for (i in 1:nrow(myDF)) {
+
+        if (is.na(myDF$CENTRO.DE.CUSTO[i]) ) { ## this assumes that a code have run before creating the column
             myDF$CENTRO.DE.CUSTO[i] <- as.character(selectInput(paste0("sel", i), "", choices = CC, width = "100px"))
-          }
+        }
           
-          if (is.na(myDF$RUBRICA[i]) ) { ## this assumes that a code have run before creating the column
+        if (is.na(myDF$RUBRICA[i]) ) { ## this assumes that a code have run before creating the column
             myDF$RUBRICA[i] <- as.character(selectInput(paste0("sel2", i), "", choices = CCrub, width = "100px"))
           
-              }
         }
+      }
         
-        myDF
+      myDF
     })
     
-
     output$contents <- DT::renderDataTable(
         data(), escape = FALSE, selection = 'none', server = FALSE,
         options = list(dom = 't', paging = FALSE, ordering = FALSE),
@@ -93,40 +99,42 @@ server <- function(input, output) {
       str(sapply(1:nrow(data()), function(i) input[[paste0("sel2", i)]]))
     })
     
-    
     output$downloadData <- downloadHandler(
     
         filename = function() {
-            paste("test.csv")
+            #paste("test.csv")
+          paste0(letters[as.integer(levels(data()$Mês))], "_", 
+                month(month(as.integer(levels(data()$Mês))), label = TRUE),".csv")
         },
         content = function(file) {
+            
+          RUBRICA <- unlist(str_extract_all(selector_R(), "[A-Z-Ç Ã Á É Ó Õ - . ( ) /]+")) %>% trimws()
+          RUBRICA <- RUBRICA[2:length(RUBRICA)]
+          RUBRICA <- RUBRICA[grep("[A-Z]", RUBRICA)]
+            
+          tmpVector <- grep("NULL", RUBRICA)
+            
+          for (i in 1:length(tmpVector)){ ## extract auto value and merge it to the selected vector
+            RUBRICA[tmpVector[i]] <- data()$RUBRICA[tmpVector[i]]
+          }
+            
           
-            RUBRICA <- unlist(strsplit(selector_R(), " "))
-            RUBRICA <- as.character(RUBRICA[4:length(RUBRICA)])
-            RUBRICA <- RUBRICA[grep("[A-Z]", RUBRICA)]
+          
+          CENTRO.DE.CUSTO <- unlist(str_extract_all(selector_CC(), "[A-Z-Ç Ã Á É Ó Õ - . ( ) /]+")) %>% trimws()
+          CENTRO.DE.CUSTO <- CENTRO.DE.CUSTO[2:length(CENTRO.DE.CUSTO)]
+          CENTRO.DE.CUSTO <- CENTRO.DE.CUSTO[grep("[A-Z]", CENTRO.DE.CUSTO)]
             
-            tmpVector <- grep("NULL", RUBRICA)
+          tmpVector <- grep("NULL", CENTRO.DE.CUSTO)
             
-            for (i in 1:length(tmpVector)){ ## extract auto value and merge it to the selected vector
-              RUBRICA[tmpVector[i]] <- data()$RUBRICA[tmpVector[i]]
-            }
-            
-            CENTRO.DE.CUSTO <- unlist(strsplit(selector_CC(), " "))
-            CENTRO.DE.CUSTO <- as.character(CENTRO.DE.CUSTO[4:length(CENTRO.DE.CUSTO)])
-            CENTRO.DE.CUSTO <- CENTRO.DE.CUSTO[grep("[A-Z]", CENTRO.DE.CUSTO)]
-            
-            tmpVector <- grep("NULL", CENTRO.DE.CUSTO)
-            
-            for (i in 1:length(tmpVector)){ ## extract auto value and merge it to the selected vector
-              CENTRO.DE.CUSTO[tmpVector[i]] <- data()$CENTRO.DE.CUSTO[tmpVector[i]]
-            }
+          for (i in 1:length(tmpVector)){ ## extract auto value and merge it to the selected vector
+            CENTRO.DE.CUSTO[tmpVector[i]] <- data()$CENTRO.DE.CUSTO[tmpVector[i]]
+          }
 
-            finalDF <- bind_cols(data() %>% 
-                                   select(-CENTRO.DE.CUSTO,-RUBRICA), 
-                                 CENTRO.DE.CUSTO = CENTRO.DE.CUSTO,
-                                 RUBRICA = RUBRICA)
-            write.csv(finalDF, file, row.names = FALSE)
-            
+          finalDF <- bind_cols(data() %>% 
+                                 select(-CENTRO.DE.CUSTO,-RUBRICA), 
+                               CENTRO.DE.CUSTO = CENTRO.DE.CUSTO,
+                               RUBRICA = RUBRICA)
+          write.csv(finalDF, file, row.names = FALSE)
         }
     )
 
